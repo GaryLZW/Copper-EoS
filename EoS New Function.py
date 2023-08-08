cb456c6ff68dd7860156959df1f399e4bec87cf0
lucahowles <141418253+lucahowles@users.noreply.github.com>
2023-08-08 14:21:07 +0100

    Update EoS New Function.py

--- EoS New Function.py
+++ EoS New Function.py
@@ -1,49 +1,74 @@
-def workflow_slab():
-   def EOS():
-       import numpy as np
+def EOS(name='Cu', crystalstructure='fcc', a=3.615):
+    import numpy as np
 
-       from ase import Atoms
-       from ase.io.trajectory import Trajectory
-       from ase.calculators.emt import EMT
+    from ase import Atoms
+    from ase.build import bulk
+    from ase.io.trajectory import Trajectory
+    from ase.calculators.emt import EMT
 
-       a = 3.615  # approximate lattice constant
-       b = a / 2
-       cu = Atoms('Cu',
-                  cell=[(0, b, b), (b, 0, b), (b, b, 0)],
-                  pbc=1,
-                  calculator=EMT())  # use EMT potential
-       cell = cu.get_cell()
-       traj = Trajectory('Cu.traj', 'w')
-       for x in np.linspace(0.95, 1.05, 5):
-           cu.set_cell(cell * x, scale_atoms=True)
-           cu.get_potential_energy()
-           traj.write(cu)
+    a = a  # approximate lattice constant
+    b = a / 2
+    elem = bulk(name, crystalstructure, a)
+    elem.calc = EMT()
+    cell = elem.get_cell()
+    traj = Trajectory('elem.traj', 'w')
+    for x in np.linspace(0.95, 1.05, 5):
+        elem.set_cell(cell * x, scale_atoms=True)
+        elem.get_potential_energy()
+        traj.write(elem)
 
-       from ase.io import read
-       from ase.units import kJ
-       from ase.eos import EquationOfState
-       configs = read('Cu.traj@0:5')  # read 5 configurations
-       # Extract volumes and energies:
-       volumes = [cu.get_volume() for cu in configs]
-       energies = [cu.get_potential_energy() for cu in configs]
-       eos = EquationOfState(volumes, energies)
-       v0, e0, B = eos.fit()
-       print(B / kJ * 1.0e24, 'GPa')
-       eos.plot('Cu-eos.png')
-       vol_cubic = (v0 * 4)
-       a = vol_cubic ** (1 / 3)
-       print(a)
+    from ase.io import read
+    from ase.units import kJ
+    from ase.eos import EquationOfState
+    configs = read('elem.traj@0:5')  # read 5 configurations
+    # Extract volumes and energies:
+    volumes = [elem.get_volume() for elem in configs]
+    energies = [elem.get_potential_energy() for elem in configs]
+    eos = EquationOfState(volumes, energies)
+    v0, e0, B = eos.fit()
+    print(B / kJ * 1.0e24, 'GPa')
+    eos.plot('elem-eos.png')
+    vol_cubic = (v0 * 4)
+    a = vol_cubic ** (1 / 3)
+    print(a)
+    return a
 
-   #bulk
-   def generate_slabs(bulk, Cu,  fcc, a, cubic=True):
-       from ase.build import bulk
-       from ase.visualize import view
 
-       if bulk:
-        a1 = bulk('Cu', 'fcc', a=a)
-        view(a1)
+def generate_slabs(name, lattice, a, layers=3, vacuum=10, indicesList=[(1, 0, 0), (0, 1, 0), (0, 0, 1),
+                                                                       (1, 1, 1), (2, 1, 1), (1, 2, 1),
+                                                                       (1, 1, 2)]):
+    from ase.build import bulk
+    a1 = bulk(name=name, crystalstructure=lattice, a=a, cubic=True)
 
-   EOS()
-   generate_slabs(bulk=True, Cu='Cu', fcc='fcc', a=a, cubic=True)
+    from ase.build import surface
+    slabList = []
+    for indices in indicesList:
+        # create slabs
+        slab = surface(lattice=a1, indices=indices, layers=layers, vacuum=vacuum)
+        slabList.append((indices, slab))
 
-workflow_slab()
+    # All slabs created
+    return slabList
+
+
+def workflow_slab(name, crystalstructure, a, layers, vacuum, indices):
+    a_eos = EOS(name, crystalstructure, a)
+
+    slabs = generate_slabs(name, crystalstructure, a_eos, layers, vacuum, indices)
+
+    from ase.io.trajectory import Trajectory
+    traj = Trajectory('slabs.traj', 'w')
+    for slab in slabs:
+        traj.write(slab[1])
+
+    return slabs
+
+
+slabs = workflow_slab(name='Cu', crystalstructure='fcc', a=3.615, layers=3, vacuum=10.0,
+                      indices=[(1, 0, 0), (1, 1, 0), (1, 1, 1)])
+
+from ase.visualize import view
+from ase.io import read
+slabs0 = read('slabs.traj@:')
+view(slabs0)
+print(slabs[-1][0])
